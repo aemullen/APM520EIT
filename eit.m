957a0fb0769a5d990cecdda596841951f4fadde7
Roberto Alvarez <rca@asu.edu>
2020-03-24 10:00:26 -0700

    Created eit function.

--- eit.m
+++ eit.m
@@ -1,92 +1,51 @@
-% 1- Add noise to boundary conditions
-% 2- Guess sigma zero (initial value for conductivity)
-% 3- Find u0 (voltage) using forward model
-% 4- Calculate r0 (residual) (ri = f(sigma i, ui) - yi )
-% 5- Using u0, update sigma:
-%         sigma 1 = sigma 0 + ((J'J)^-1) * J'  * r0
+%% Electrical Impedence Tomography
+% Roberto Alvarez, Bechir Amdouni, Andrew Mullen, Trevor Reckell, Shan Zhu
+
+% Outline
+% 1 - Add noise to boundary conditions
+% 2 - Guess sigma zero (initial value for conductivity)
+% 3 - Find u0 (voltage) using forward model
+% 4 - Calculate residual r_0 (r_i = f(sigma i, ui) - yi )
+% 5 - Using u0, update sigma:
+%         sigma_i = sigma_0 + ((J'J)^-1) * J'  * r_i
 % 6 - Find u1 using forward model
-% 7- Find r1 and so on
+% 7 - Find r1 and so on
 
 %% Inverse problem
-N = 50;
-u = zeros(N+1, N+1);
-% Boudary conditions
-k = (1:N+1)';
-n = normrnd(1,0.09,N+1,1);
-u(N+1,k) = normrnd(1,0.09,N+1,1); % nonzero BC 
-u(1,k) = normrnd(1,0.09,N+1,1);
-u(k,1) = normrnd(1,0.09,N+1,1); % nonzero BC 
-u(k,N+1) = normrnd(1,0.09,N+1,1);
+% N = 50;
 
-
+function [u, sigma] = eit(N, tol, maxiter)
 % Gauss Newton Method
-
-
+u = zeros(N+1, N+1);
 sigma = zeros(N+1, N+1);
-% step one: initial guess sigma 0
-for i= 1:N+1
+residual = 0;
+iter = 0;
+% step one: boudary conditions
+u(N+1, :) = normrnd(1, 0.09, N+1, 1);
+u(1, :) = normrnd(1, 0.09, N+1, 1);
+u(:, 1) = normrnd(1, 0.09, N+1, 1);
+u(:, N+1) = normrnd(1, 0.09, N+1, 1);
+% step two: initial guess for sigma
+for i = 1:N+1
     for j = 1:N+1
         sigma(i,j) = 1;
-        %u = Forward(N,sigma);
-        
     end
-     
 end
-u_sol = Forward(N,sigma,u);
 
-
-
-
-
-%%
-function u_s = Forward(N, sigma,u)
-%laplace1(N) solves the 2D Laplace equation 
-%        u_xx + u_yy = 0 
-%    on the unit square with N dx by N dy, with dx = dy = h. 
-%    Gridpoints are labeled i = 1,...,N+1 and j = 1,...,N+1.
-%    In u(i,j), i labels x and j labels y.
-%    BCs are Dirichlet with u = 0 on boundary except u(x=1,y) = 1.
-%    Uses Gauss-Seidel iteration.
-
-%tic
-h = 1/N; % dx = dy = h
-k = (1:N+1)';
-x = (k-1)/N;
-y = (k-1)/N;
-% u = zeros(N+1,N+1);
-% u(N+1,k) = 1; % nonzero BC 
-% u(1,k) = 1;
-% u(k,1) = 1; % nonzero BC 
-% u(k,N+1) = 1;
-
-sum = 0;
-%sigma = 0.39;
-% for i = 2:N
-%     for j = 2:N
-%         residual = sigma(i,j)*u(i,j)+sigma(i+1,j)*u(i+1,j)+sigma(i-1,j)*u(i-1,j)+sigma(i,j+1)*u(i,j+1)+sigma(i,j-1)*u(i,j-1); 
-%         sum = sum + abs(residual);
-%     end
-% end
-normresidual = 0.5; % could divide by (N-1)^2 here and below in line 39
-EPSILON = 10^-5*h^2*normresidual;
-iter = 0;
-while normresidual > EPSILON
-    iter = iter + 1;
-    sum = 0;
-    for i = 2:N
-        for j = 2:N
-            residual = -4*sigma(i,j)*u(i,j)+sigma(i+1,j)*u(i+1,j)+sigma(i-1,j)*u(i-1,j)+sigma(i,j+1)*u(i,j+1)+sigma(i,j-1)*u(i,j-1);
-            sum = sum + abs(residual);
-            u(i,j) = u(i,j) + residual/4;  
-        end
-    end
-    normresidual = sum;
+while residual > tol
+% step three: solve for the voltage using forward model
+u_sol = forward(N, sigma, u);
+% step four: compute residual
+residual = abs(u_sol - u);
+% step five: update sigma
+J = jac(residual);
+sigma = sigma + (J'*J)^(-1)*J'*residual;
+if iter == maxiter
+    break
 end
-iterations = iter
-%toc
-
-%u = u'; % transpose to plot
-u_s = u
+iter = iter + 1;
+end
+end
 % figure
 % surf(x,y,u);
 % shading flat;
@@ -104,4 +63,49 @@ u_s = u
 % colormap jet;
 % colorbar;
 
+%% Solve the forward problem
+function u = forward(N, sigma, u)
+% Solve the 2D forward model,
+% 
+%   div(sigma grad(u)) = 0,
+%
+% on the unit square with N dx by N dy, with dx = dy = h. 
+% Gridpoints are labeled i = 1,...,N+1 and j = 1,...,N+1.
+% In u(i,j), i labels x and j labels y.
+% Uses Gauss-Seidel iteration.
+
+h = 1/N;
+k = (1:N+1)';
+x = (k-1)/N;
+y = (k-1)/N;
+
+sum = 0;
+for i = 2:N
+    for j = 2:N
+        A = sigma(i,j)*u(i,j)+sigma(i+1,j)*u(i+1,j)+...
+            sigma(i-1,j)*u(i-1,j)+sigma(i,j+1)*u(i,j+1)+...
+            sigma(i,j-1)*u(i,j-1); 
+        sum = sum + abs(A);
+    end
+end
+normresidual = 0.5;
+EPSILON = 10^-5*h^2*normresidual;
+while normresidual > EPSILON
+    sum = 0;
+    for i = 2:N
+        for j = 2:N
+            A = -4*sigma(i,j)*u(i,j)+sigma(i+1,j)*u(i+1,j)+...
+                sigma(i-1,j)*u(i-1,j)+sigma(i,j+1)*u(i,j+1)+...
+                sigma(i,j-1)*u(i,j-1);
+            sum = sum + abs(A);
+            u(i,j) = u(i,j) + A/4;  
+        end
+    end
+    normresidual = sum;
+end
+end
+
+function J = jac(residual)
+% Calculate Jacobian with respect to spatial derivatives.
+J = zeros(size(residual));
 end
